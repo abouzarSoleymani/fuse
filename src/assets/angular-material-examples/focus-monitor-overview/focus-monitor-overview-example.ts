fc6f6e5d6d69a33e5dbabfe546fbc6402327aaea
abouzarSoleymani <iras.user@gmail.com>
2019-06-17 22:51:50 +0430

    fuse update angular 8.0.1

---
+++ src/assets/angular-material-examples/focus-monitor-overview/focus-monitor-overview-example.ts
@@ -0,0 +1,51 @@
+import {FocusMonitor, FocusOrigin} from '@angular/cdk/a11y';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  NgZone,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import {MatAccordion} from '@angular/material';
+
+/** @title Monitoring focus with FocusMonitor */
+@Component({
+  selector: 'focus-monitor-overview-example',
+  templateUrl: 'focus-monitor-overview-example.html',
+  styleUrls: ['focus-monitor-overview-example.css']
+})
+export class FocusMonitorOverviewExample implements OnDestroy, OnInit {
+  @ViewChild('element', { static: true}) element: ElementRef<HTMLElement>;
+  @ViewChild('subtree', { static: true}) subtree: ElementRef<HTMLElement>;
+
+  elementOrigin = this.formatOrigin(null);
+  subtreeOrigin = this.formatOrigin(null);
+
+  constructor(private focusMonitor: FocusMonitor,
+              private cdr: ChangeDetectorRef,
+              private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.focusMonitor.monitor(this.element)
+        .subscribe(origin => this.ngZone.run(() => {
+          this.elementOrigin = this.formatOrigin(origin);
+          this.cdr.markForCheck();
+        }));
+    this.focusMonitor.monitor(this.subtree, true)
+        .subscribe(origin => this.ngZone.run(() => {
+          this.subtreeOrigin = this.formatOrigin(origin);
+          this.cdr.markForCheck();
+        }));
+  }
+
+  ngOnDestroy() {
+    this.focusMonitor.stopMonitoring(this.element);
+    this.focusMonitor.stopMonitoring(this.subtree);
+  }
+
+  formatOrigin(origin: FocusOrigin): string {
+    return origin ? origin + ' focused' : 'blurred';
+  }
+}
---
+++ src/assets/angular-material-examples/sidenav-disable-close/sidenav-disable-close-example.ts
@@ -0,0 +1,22 @@
+import {Component, ViewChild} from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
+import {MatAccordion} from '@angular/material';
+
+/** @title Sidenav with custom escape and backdrop click behavior */
+@Component({
+  selector: 'sidenav-disable-close-example',
+  templateUrl: 'sidenav-disable-close-example.html',
+  styleUrls: ['sidenav-disable-close-example.css'],
+})
+export class SidenavDisableCloseExample {
+  @ViewChild('sidenav', { static: true}) sidenav: MatSidenav;
+
+  reason = '';
+
+  close(reason: string) {
+    this.reason = reason;
+    this.sidenav.close();
+  }
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+}
